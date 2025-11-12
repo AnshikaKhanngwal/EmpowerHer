ffff549265728d39ca9a43227a306fba24ba4cbb
AnshikaKhanngwal <officialanshikakhanngwal@gmail.com>
2025-11-12 13:59:55 +0530

    day 5 q11

---
+++ Module2/Day5_github/index.js
@@ -0,0 +1,17 @@
+let num=2
+
+function checkprime(num){
+    let count=0
+    for(let i=1;i<=num;i++){
+        if(num%1==0){
+            count++
+        }
+    }
+     if(count==2){
+        return true
+     }else {
+        return false
+     }
+}
+
+console.log(checkprime(num))
