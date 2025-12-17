82dc40cc5b743903301185f0bd50f93770a8c0a6
AnshikaKhanngwal <officialanshikakhanngwal@gmail.com>
2025-12-17 15:36:57 +0530

    changes

--- Module2/Module2/Day20_Standard Project/components/footer.js
+++
@@ -1,6 +0,0 @@
-export function footer() {
-    return `
-        <footer style="padding: 10px; background: #eee; text-align:center;">
-            <p>© 2025 Your App. All rights reserved.</p>
-        </footer>`;
-}
--- Module2/Module2/Day20_Standard Project/components/navbar.js
+++
@@ -1,8 +0,0 @@
-export function navbar() {
-    return `
-        <nav style="padding: 10px; background: #333; color: white;">
-            <a href="index.html" style="color:white; margin-right:20px;">Home</a>
-            <a href="signup.html" style="color:white; margin-right:20px;">Signup</a>
-            <a href="login.html" style="color:white;">Login</a>
-        </nav>`;
-}
