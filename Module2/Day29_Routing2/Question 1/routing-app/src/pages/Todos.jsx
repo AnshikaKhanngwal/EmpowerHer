0561ed6ea9fff0d1d9658cd1e3f4ff1e88327ef4
AnshikaKhanngwal <officialanshikakhanngwal@gmail.com>
2025-12-25 21:52:00 +0530

    Routing2

---
+++ Module2/Day29_Routing2/Question 1/routing-app/src/pages/Todos.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Todos = () => {
+  const [todos, setTodos] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.json())
+      .then((data) => setTodos(data.slice(0, 10)));
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("isLoggedIn");
+    navigate("/login");
+  };
+
+  return (
+    <div className="container">
+      <h2>Todos</h2>
+      <button onClick={handleLogout}>Logout</button>
+
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <li
+            key={todo.id}
+            onClick={() => navigate(`/todos/${todo.id}`)}
+          >
+            {todo.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Todos;
